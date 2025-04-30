9b8c6fcc2729cae6bfc9fd4b06faba8607dbc839
Wael Chouchani <waelchouchani@gmail.com>
2025-04-30 23:55:51 +0100

    Test

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ function authenticateToken(req, res, next) {
         console.log("⛔ Aucun token trouvé");
         return res.status(401).json({ error: "Token manquant" });
     }
-
+//test
     // Vérification du token
     jwt.verify(token, process.env.JWT_SECRET || "SECRET_KEY", (err, user) => {
         if (err) {
